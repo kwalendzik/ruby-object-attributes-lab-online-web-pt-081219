28c499fdcbe4c1222463727b8bd4c72e07e84808
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-09 02:49:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+ 
+ def initialize(name)
+   @name = name
+   @breed = breed
+ end
+ 
+ def name
+   @name
+ end
+ 
+ def name=(new_name)
+   @name = new_name
+ end
+ 
+ def breed
+   @breed
+ end
+ 
+ def breed=(new+breed)
+   @breed = new_breed
+ end
+ 
+end
